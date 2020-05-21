4bf0e4a65696f5dd7178dcc9d7ec7c5303159933
Wilmer420 <wrodriguez420@yahoo.com>
2020-05-21 14:09:40 -0400

    Final Project Scripts

---
+++ ObjectDestroyer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+	public GameObject inventory, diamond;
+
+	public Text gemsText;
+
+    void Update()
+    {
+       
+		gemsText.text = "Inventory: " + inventory.transform.childCount;
+
+        // 1. Test for mouse click
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            // 2. get mouse position in world space
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
+
+            //3. get direction vector from camera position to mouse position in world space
+            Vector3 direction = worldMousePosition - Camera.main.transform.position;
+
+            RaycastHit hit;
+
+            //4. cast a ray from the camera in the given direction
+            if (Physics.Raycast(Camera.main.transform.position, direction, out hit, 100f))
+            {
+
+                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.green, 0.5f);
+
+                // 5. Destroy game object
+                if (hit.collider.gameObject.tag == "Enemy")
+                {
+					//  Destroy(hit.collider.gameObject);
+					hit.collider.gameObject.transform.parent = inventory.transform;
+					hit.collider.gameObject.SetActive(false);
+					//	hit.collider.gameObject.GetComponent<Renderer>().enabled = false;
+					diamond.transform.localScale += new Vector3(1, 1, 1);
+
+                }
+
+            }
+            else
+            {
+                Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.red, 0.5f);
+            }
+            if (inventory.transform.childCount == 50)
+			{
+				SceneManager.LoadScene("GameClear");
+			}
+			//if (Input.GetKeyDown(KeyCode.Z))
+			//{
+			//	Debug.Log("inventory: " + inventory.transform.childCount);
+
+			//	bool grow = true;
+
+			//	diamond.transform.localScale += new Vector3(inventory.transform.childCount, inventory.transform.childCount, inventory.transform.childCount);
+
+			//	if (grow)
+			//	{
+			//		foreach (Transform child in inventory.transform)
+			//		{
+			//			Destroy(child.gameObject);
+			//		}
+			//		grow = false;
+			//	}
+			//}
+
+		}
+
+    }
+}
+
+
---
+++ PlayerLook.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    // mouseSensitivty can be modified in Unity to adjust the speed of the mouse.
+    // playerBody must be referenced in Unity to allow the camera rotation to rotate the player body.
+    public float mouseSensitivity;
+
+    public Transform playerBody;
+    public float rotateSpeed = 100f;
+
+    // Rotation on the x-axis (up and down) uses a private float to limit movement.
+    float xRotation = 0f;
+
+    // Variable to enable or disable the cursor
+    public static bool hideCursor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // // This makes the cursor disappear once the game begins to avoid clicking outside the window by mistake.
+        // // Press Esc key in the game window to bring the cursor back.
+        hideCursor = true;
+        //if (MainMenu.mouseSet < 300)
+        //{
+        //    mouseSensitivity = 300;
+        //}
+        //else
+        //{
+        //    mouseSensitivity = MainMenu.mouseSet;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(mouseSensitivity);
+
+        // // Hide the cursor during gameplay or menus
+        //if (hideCursor)
+        //{
+        //    Cursor.lockState = CursorLockMode.Locked;
+        //}
+        //else
+        //{
+        //    Cursor.lockState = CursorLockMode.None;
+        //}
+
+        // Private variables to gather the mouse's x and y axis.
+        // This is multiplied by the mouseSensitivity to control the speed and 
+        // by Time.deltaTime to rotate independent of current framerate.
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // xRotation is used to calculate the angle of rotation along the x-axis (up and down).
+        // Mathf.Clamp() takes a value (xRotation) and limits its output between two values (-90 and 90).
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, 0f, 0f);
+
+        // For the y rotation (up and down) we use transform.localRotation which takes a Vector3 value.
+        // Quaternion is responsible for rotation in Unity and Euler gives us outpun in angles.
+        // xRotation is the only value that we want to change so we use 0f for y and z.
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Vector3.up * mouseX allows the playerBody.Rotate() to rotate along the y-axis (left and right).
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     playerBody.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        // }
+
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     playerBody.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+        // }
+
+    }
+}
+
